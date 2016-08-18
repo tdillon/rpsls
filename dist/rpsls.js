2b442754ace65dff72bdfc9cf569b8bbd21d3b35
tdillon <tdillon@users.noreply.github.com>
2016-08-18 15:57:58 -0400

    Stubbed method/properties on class

--- dist/rpsls.js
+++ dist/rpsls.js
@@ -8,6 +8,49 @@ System.register([], function(exports_1, context_1) {
             RockPaperScissorsLizardSpock = (function () {
                 function RockPaperScissorsLizardSpock() {
                 }
+                Object.defineProperty(RockPaperScissorsLizardSpock, "TIE", {
+                    get: function () { return -1; },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(RockPaperScissorsLizardSpock, "PLAYER1", {
+                    get: function () { return -1; },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(RockPaperScissorsLizardSpock, "PLAYER2", {
+                    get: function () { return -1; },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(RockPaperScissorsLizardSpock, "ROCK", {
+                    get: function () { return -1; },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(RockPaperScissorsLizardSpock, "PAPER", {
+                    get: function () { return -1; },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(RockPaperScissorsLizardSpock, "SCISSORS", {
+                    get: function () { return -1; },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(RockPaperScissorsLizardSpock, "LIZARD", {
+                    get: function () { return -1; },
+                    enumerable: true,
+                    configurable: true
+                });
+                Object.defineProperty(RockPaperScissorsLizardSpock, "SPOCK", {
+                    get: function () { return -1; },
+                    enumerable: true,
+                    configurable: true
+                });
+                RockPaperScissorsLizardSpock.play = function (p1, p2) {
+                    return null;
+                };
                 return RockPaperScissorsLizardSpock;
             }());
             exports_1("RockPaperScissorsLizardSpock", RockPaperScissorsLizardSpock);
--- src/rpsls.ts
+++ src/rpsls.ts
@@ -1,3 +1,16 @@
 export class RockPaperScissorsLizardSpock {
 
+  static get TIE() { return -1; }
+  static get PLAYER1() { return -1; }
+  static get PLAYER2() { return -1; }
+
+  static get ROCK() { return -1; }
+  static get PAPER() { return -1; }
+  static get SCISSORS() { return -1; }
+  static get LIZARD() { return -1; }
+  static get SPOCK() { return -1; }
+
+  static play(p1: number, p2: number): { outcome: number, result: string } {
+    return null;
+  }
 }
