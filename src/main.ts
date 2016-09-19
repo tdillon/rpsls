a882a729bf1db2e26eaf39c5eb0b3b44ba7d8a72
tdillon <tdillon@users.noreply.github.com>
2016-09-19 12:00:44 -0400

    adding tally of win lose tie

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import R from 'rpsls'
 
 
 let btns = [...<NodeListOf<HTMLElement>>document.querySelectorAll('i')];
+let [spanWin, spanLose, spanTie] = <NodeListOf<HTMLElement>>document.querySelectorAll('span');
 let result = <HTMLDivElement>document.querySelector('div');
 let outcomeCSS = ['tie', 'win', 'lose'];
 
@@ -13,4 +14,15 @@ btns.forEach(i => i.addEventListener('click', a => {
     r.className = outcomeCSS[g.outcome];
     r.textContent = `${g.result}`;
     result.insertBefore(r, result.firstChild);
+
+    switch (g.outcome) {
+        case R.TIE:
+            spanTie.textContent = (+spanTie.textContent + 1).toString();
+            break;
+        case R.PLAYER1:
+            spanWin.textContent = (+spanWin.textContent + 1).toString();
+            break;
+        case R.PLAYER2:
+            spanLose.textContent = (+spanLose.textContent + 1).toString();
+    }
 }));
